b4cedcf8fa97bdf1d9188f00390f8d4514de63ec
ruslan.zaslotskyi <ruslan.zaslotskyi@uklon.com.ua>
2024-01-21 20:10:05 +0200

    refactoring calculator

--- lesson3.py
+++ lesson3.py
@@ -2,20 +2,20 @@
 first_number = int(input("Enter first number: "))
 math_operation = input("Enter mathematical operation: ")
 second_number = int(input("Enter second number "))
-if math_operation == "*":
-    multiplication = first_number * second_number
-    print(multiplication)
-elif math_operation == "+":
-    addition = first_number + second_number
-    print(addition)
-elif math_operation == "-":
-    subtraction = first_number - second_number
-    print(subtraction)
-elif math_operation == "/" and second_number != 0:
-    division = first_number / second_number
-    print(round(division))
-else:
+if math_operation == "/" and second_number == 0:
     print("You cannot divide by zero")
+elif math_operation != "-" and math_operation != "+" and math_operation != "*" and math_operation != "/":
+     print(f"{math_operation} is not a mathematical operation")
+else:
+    if math_operation == "*":
+        result = first_number * second_number
+    elif math_operation == "+":
+        result = first_number + second_number
+    elif math_operation == "-":
+        result = first_number - second_number
+    elif math_operation == "/" and second_number != 0:
+        result = first_number / second_number
+    print(result)
 
 #2
 my_list = [12, 3, 4, 10]
@@ -27,6 +27,15 @@ if len(my_list) != 0:
     print(second_list)
 else:
     print(my_list)
+
+
+#2 best option
+my_list = [12, 3, 4, 10]
+
+if len(my_list) > 1:
+    my_list.insert(0, my_list.pop())
+
+print(my_list)
 
 
 
