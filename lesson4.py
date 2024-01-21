46825e81e9a8bf54f84e3c1e072231d71d48f844
ruslan.zaslotskyi <ruslan.zaslotskyi@uklon.com.ua>
2024-01-21 18:55:56 +0200

    Add homework for lesson 2/Rename "Hello" to "lesson1"

--- lesson4.py
+++ lesson4.py
@@ -1,12 +1,12 @@
-# #1
-# my_list = [9, 0, 7, 31, 0, 45, 0, 45, 0, 45, 0, 0, 96, 0]
-# second_list = []
-# zero = my_list.count(0)
-# for index in range(len(my_list)):
-#     if my_list[index] != 0:
-#         second_list.append(my_list[index])
-# second_list.extend([0] * zero)
-# print(second_list)
+#1
+my_list = [9, 0, 7, 31, 0, 45, 0, 45, 0, 45, 0, 0, 96, 0]
+second_list = []
+zero = my_list.count(0)
+for index in range(len(my_list)):
+    if my_list[index] != 0:
+        second_list.append(my_list[index])
+second_list.extend([0] * zero)
+print(second_list)
 
 
 
