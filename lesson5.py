1ccba6f37d1d046af37dbf73c371ffb9873a2432
ruslan.zaslotskyi <ruslan.zaslotskyi@uklon.com.ua>
2024-01-28 20:59:00 +0200

    add " " in ex1

--- lesson5.py
+++ lesson5.py
@@ -9,7 +9,7 @@ def can_be_value(value):
     if not value[0].isnumeric():
         if not value in reserved:
             for symbols in value:
-                if symbols in spec_char or symbols.isupper() or "__" in value:
+                if symbols in spec_char or symbols.isupper() or "__" in value or " " in value:
                     return False
             return True
     return False
@@ -21,31 +21,31 @@ print(can_be_value(value))
 #2
 
 
-while True:
-    first_number = int(input("Enter first number: "))
-    math_operation = input("Enter mathematical operation: ")
-    second_number = int(input("Enter second number "))
-    if math_operation == "/" and second_number == 0:
-        print("You cannot divide by zero")
-    elif math_operation != "-" and math_operation != "+" and math_operation != "*" and math_operation != "/":
-        print(f"{math_operation} is not a mathematical operation")
-    else:
-        if math_operation == "*":
-            result = first_number * second_number
-        elif math_operation == "+":
-            result = first_number + second_number
-        elif math_operation == "-":
-            result = first_number - second_number
-        elif math_operation == "/" and second_number != 0:
-            result = first_number / second_number
-        print(result)
-    repeat = input("Do you want continue?(yes/no): ")
-    if repeat == "yes":
-        continue
-    elif repeat == "no":
-        break
-    else:
-        break
+# while True:
+#     first_number = int(input("Enter first number: "))
+#     math_operation = input("Enter mathematical operation: ")
+#     second_number = int(input("Enter second number "))
+#     if math_operation == "/" and second_number == 0:
+#         print("You cannot divide by zero")
+#     elif math_operation != "-" and math_operation != "+" and math_operation != "*" and math_operation != "/":
+#         print(f"{math_operation} is not a mathematical operation")
+#     else:
+#         if math_operation == "*":
+#             result = first_number * second_number
+#         elif math_operation == "+":
+#             result = first_number + second_number
+#         elif math_operation == "-":
+#             result = first_number - second_number
+#         elif math_operation == "/" and second_number != 0:
+#             result = first_number / second_number
+#         print(result)
+#     repeat = input("Do you want continue?(yes/no): ")
+#     if repeat == "yes":
+#         continue
+#     elif repeat == "no":
+#         break
+#     else:
+#         break
 
 
 
