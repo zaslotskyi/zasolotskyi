c7feb3745f1691e37a442e0c064bdd6b41c519b8
ruslan.zaslotskyi <ruslan.zaslotskyi@uklon.com.ua>
2024-02-04 19:21:48 +0200

    Add lesson7 homework

--- lesson7.py
+++ lesson7.py
@@ -38,6 +38,7 @@ def correct_sentence(text):
 
 
 
+
 assert correct_sentence("greetings, friends") == "Greetings, friends.", 'Test1'
 assert correct_sentence("hello") == "Hello.", 'Test2'
 assert correct_sentence("Greetings. Friends") == "Greetings. Friends.", 'Test3'
