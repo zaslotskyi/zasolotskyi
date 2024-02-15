6ea47aa011c9263220bf34f3767c7770162f19b3
ruslan.zaslotskyi <ruslan.zaslotskyi@uklon.com.ua>
2024-02-15 20:26:41 +0200

    Add lesso10.py homework

---
+++ lesson10.py
@@ -0,0 +1,42 @@
+#1
+from inspect import isgenerator
+import sys
+def pow(x):
+    return x ** 2
+
+def some_gen(begin, end, func):
+    current_value = begin
+    for i in range(end):
+        yield current_value
+        current_value = func(current_value)
+
+gen = some_gen(2, 4, pow)
+
+
+assert isgenerator(gen) == True, 'Test1'
+assert list(gen) == [2, 4, 16, 256], 'Test2'
+print('OK')
+
+
+
+#2
+
+def is_even(digit):
+    return digit % 2 == 0
+
+
+assert is_even(2) == True, 'Test1'
+assert is_even(5) == False, 'Test2'
+assert is_even(0) == True, 'Test3'
+print('OK')
+
+
+
+
+
+
+
+
+
+
+
